f9d7f776258f8bd81cb1afd9d05ca958b7b13cb6
Tomás Laus <tomaslaus99@gmail.com>
2024-04-08 18:36:40 -0300

    <ADD> Auth Page Styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,10 +14,10 @@ const Navbar: React.FC<NavbarProps> = () => {
   return (
     <div className='flex items-center justify-between sm:px-12 px-2 md:px-24'>
       <Link href='/' className='flex items-center justify-center h-20'>
-        <Image width={200} height={200} src='/solveit-logo.png' alt='logo' className='h-full' />
+        <Image width={150} height={50} src='/solveit-logo-full.png' alt='logo' />
       </Link>
       <div className='flex items-center'>
-        <button className='bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange border-2 border-transparent transition duration-300 ease-in-out'
+        <button className=' text-white px-2 py-1 sm:px-4 border-[#B13C3D] rounded-md text-sm font-medium hover:text-white  hover:bg-[#B13C3D] hover:border-1 hover:border-[#6d2a2b] border-2  transition duration-300 ease-in-out'
         onClick={handleClick}
         >
           Login
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -10,6 +10,9 @@ import { auth } from '@/firebase/firebase';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
+import home4 from '../../../public/home-4.png';
+import home2 from '../../../public/home-2.jpg';
+import home3 from '../../../public/home-3.png';
 
 type AuthPageProps = {};
 
@@ -28,25 +31,67 @@ const AuthPage: React.FC<AuthPageProps> = () => {
   if (pageLoading) return null;
 
   return (
-    <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative animate-fadeIn'>
-      <div className='max-w-7xl mx-auto'>
-        <Navbar />
-        <div className='flex items-center justify-center h-[calc(100vh-5rem)]'>
-          <div className='text-center text-white'>
-            <h1 className='text-4xl font-bold mb-4'>
-            Welcome to your problem-solving platform!
-            </h1>
-            <p className='text-lg mb-8'>
-            Practice your problem solving skills with challenging algorithms and improve your programming logic.
+    <div className='bg-[#1D1D21] animate-fadeIn'>
+    <Navbar />
+  
+    <div className='max-w-7xl mx-auto px-4 py-12 md:py-24 text-[22px] '>
+      <div className='text-white text-center mb-12'>
+        <h1 className='font-bold mb-4'>Achieve mastery through challenge</h1>
+        <p className='mb-8'>
+          Improve your development skills by training with code problems that challenge you and
+          push your programming portfolio.
+        </p>
+        <button className='bg-[#B13C3D] hover:bg-[#a74244] text-white font-bold py-2 px-4 rounded transition ease-in-out'>
+          Start now
+        </button>
+      </div>
+  
+      <div className='grid grid-cols-1 md:grid-cols-1 gap-8 text-[32px]'>
+        {/* Sección de afilar habilidades de codificación */}
+        <div className='bg-red-500 py-7 rounded-lg flex flex-col justify-center items-center'>
+          <div className='p-5'>
+            <h2 className='font-semibold mb-2'>Sharpen your coding skills</h2>
+            <p>
+              With small coding exercises designed to help you learn different aspects of software
+              development.
             </p>
-            <Link href="/" className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 transform hover:scale-105">
-              Start now
-            </Link>
+          </div>
+          <div>
+            <Image src={home4} width={500} alt='problem solving' className='rounded' />
           </div>
         </div>
-        {authModal.isOpen && <AuthModal />}
+      </div>
+  
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-8 mt-8 text-white '>
+        {/* Sección de retroalimentación instantánea */}
+        <div className='bg-[#92909015] p-4 rounded-lg flex flex-col justify-center items-center'>
+          <div className='p-5'>
+            <h2 className='text-xl font-semibold mb-2'>Get instant feedback</h2>
+            <p>
+              Solve problems with your own coding style directly in the browser and get real-time
+              test results.
+            </p>
+          </div>
+          <div>
+            <Image src={home2} width={500} alt='problem solving' className='rounded' />
+          </div>
+        </div>
+        {/* Sección de ganar rangos y honor */}
+        <div className='bg-[#92909015] p-4 rounded-lg flex flex-col justify-center items-center'>
+          <div className='p-5'>
+            <h2 className='text-xl font-semibold mb-2'>Earn ranks and honor</h2>
+            <p>The code challenges are graded from beginner to expert level.</p>
+          </div>
+          <div>
+            <Image src={home3} width={500} alt='problem solving' className='rounded removebg'/>
+          </div>
+        </div>
       </div>
     </div>
+  
+    {authModal.isOpen && <AuthModal />}
+  </div>
+  
   );
 };
 export default AuthPage;
